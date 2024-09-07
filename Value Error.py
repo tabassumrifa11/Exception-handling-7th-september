be376950ee6380e746b43e8268ee86d2794aa79e
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-09-07 16:59:39 +0600

    first commit

---
+++ Value Error.py
@@ -0,0 +1,11 @@
+try :
+    num = int(input("Enter your number:"))
+    print(num)
+    
+    
+except ValueError as ex:
+    print("Exception:", ex)
+    
+    
+    
+print("I am outside the try block")
